f783bb3d78e7a749269da16a68b59811c7478277
Bobie <bobie@herxun.co>
2014-08-27 20:55:27 -0700

    Initial Commit

---
+++ RayTracer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RayTracer
+//
+//  Created by Bobie Chen on 8/27/14.
+//  Copyright (c) 2014 Bobie Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
